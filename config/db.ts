c5d1ae79f7fa0da9473abe328f91cef97ac401a9
septianmy <haiseptian@gmail.com>
2024-02-09 08:48:28 +0700

    Change Config DB

--- config/db.ts
+++ config/db.ts
@@ -1,12 +1,14 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
 
+const dbPort: number = parseInt(process.env.DB_PORT || '5432');
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: 5432, // Default PostgreSQL port
+  port: dbPort, // Default PostgreSQL port
 });
 
 export default pool;
